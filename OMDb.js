4009bbf340cbc3fbfc02d4a2f9d5c0fc95e53cfc
Blazze23 <l.azmar23@gmail.com>
2021-04-08 23:37:00 +0200

    updated search function

--- OMDb.js
+++ OMDb.js
@@ -1,22 +1,37 @@
 // $(document).ready( function () {
 //     $('table').DataTable();
 // } );
+$("#navbar_search").keyup(function(event) {
+    if(event.keyCode === 13) {
+       searchSetup();
+    }
+ })
+
+function searchSetup() {
+    let search = $("#navbar_search").val();
+    localStorage.setItem("search", search);
+    if(search.length == 0) {
+        Swal.fire("Error", "You have to enter a title first", "error");
+    } else {
+        location.href = "search.html";
+    }
+}
 
 function search() {
-    let search = $("#navbar_search").val();
-    if(search.length == 0) {
-        Swal.fire("Error", "You have to enter a title first", "error")
-    } else {
-        $.getJSON("https://omdbapi.com/?apikey=4194023&s="+search, function(json) {
-            console.log(json);
-            if(json.Response == "True") {
-                $("table tbody").empty();
-               $.each(json.Search, function(key, value) {
-                   $("table tbody").append("<tr><td>"+value.Title+"</td>><td>"+value.Year+"</td><td>"+value.Type+"</td><td><img src='"+value.Poster+"' class='img-fluid' style='max-width: 100px;'></td><td>"+value.imdbID+"</td><td><button class='btn btn-warning btn-xs'><i class='fas fa-info-circle pe-2'></i>Info</button></td></tr>");
-               })
-            } else {
-            Swal.fire("Error", json.Error, "error")
-            }
-        })
-    }
+        let local_search = localStorage.getItem("search");
+        console.log(local_search);
+        if(local_search != null) {
+            $.getJSON("https://omdbapi.com/?apikey=4194023&s="+local_search, function(json) {
+                console.log(json);
+                if(json.Response == "True") {
+                    $("table tbody").empty();
+                   $.each(json.Search, function(key, value) {
+                       $("table tbody").append("<tr class='align-middle'><td>"+value.Title+"</td>><td>"+value.Year+"</td><td>"+value.Type+"</td><td><img src='"+value.Poster+"' class='img-fluid' style='max-width: 60px;'></td><td>"+value.imdbID+"</td><td><button class='btn btn-warning btn-xs'><i class='fas fa-info-circle pe-2'></i>Info</button></td></tr>");
+                   })
+                } else {
+                Swal.fire("Error", json.Error, "error");
+                }
+            })
+        }
+        localStorage.removeItem("search");
 }
