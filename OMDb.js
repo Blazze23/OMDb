2bc0d83f2b8b7aa772fc680f31c97c655122f629
Blazze23 <l.azmar23@gmail.com>
2021-04-15 00:24:54 +0200

    info page added

--- OMDb.js
+++ OMDb.js
@@ -26,12 +26,44 @@ function search() {
                 if(json.Response == "True") {
                     $("table tbody").empty();
                    $.each(json.Search, function(key, value) {
-                       $("table tbody").append("<tr class='align-middle'><td>"+value.Title+"</td>><td>"+value.Year+"</td><td>"+value.Type+"</td><td><img src='"+value.Poster+"' class='img-fluid' style='max-width: 60px;'></td><td>"+value.imdbID+"</td><td><button class='btn btn-warning btn-xs'><i class='fas fa-info-circle pe-2'></i>Info</button></td></tr>");
+                       $("table tbody").append("<tr class='align-middle'><td>"+value.Title+"</td>><td>"+value.Year+"</td><td>"+value.Type+"</td><td><img src='"+value.Poster+"' class='img-fluid' style='max-width: 60px;'></td><td>"+value.imdbID+"</td><td><button class='btn btn-warning btn-xs' onclick='titleInfoSetup(\""+value.imdbID+"\")'><i class='fas fa-info-circle pe-2'></i>Info</button></td></tr>");
                    })
                 } else {
                 Swal.fire("Error", json.Error, "error");
                 }
             })
         }
-        localStorage.removeItem("search");
+        // localStorage.removeItem("search");
+}
+
+function titleInfoSetup(titleId) {
+    localStorage.setItem("titleID", titleId);
+    location.href = "info.html";
+}
+
+function titleInfo() {
+    let lokal_titleID = localStorage.getItem("titleID");
+    $.getJSON("https://omdbapi.com/?apikey=4194023&i="+lokal_titleID, function(json) {
+                console.log(json);
+                $("#titleName").html(json.Title);
+                $("#titleYear").html(json.Year);
+                $("#ratingIMDB").html(json.Ratings[0].Source);
+                $("#ratingMetta").html(json.Ratings[2].Source);
+                $("#valueIMDB").html(json.Ratings[0].Value);
+                $("#valueMetta").html(json.Ratings[2].Value);
+                $("#titlePoster").attr("src", json.Poster);
+                $("#titleDirector").html(json.Director);
+                $("#titleWriter").html(json.Writer);
+                $("#titleActors").html(json.Actors);
+                $("#titleGenre").html(json.Genre);
+                $("#titleDuration").html(json.Runtime);
+                $("#titleRated").html(json.Rated);
+                $("#titleGenre").html(json.Genre);
+                $("#titlePlot").html(json.Plot);
+                $("#titleAwards").html(json.Awards);
+                $("#titleBox").html(json.BoxOffice);
+                $("#titleCountry").html(json.Country);
+                $("#titleLanguage").html(json.Language);
+                $("#titleProduction").html(json.Production);
+            });
 }
